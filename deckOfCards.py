065190cb2a067b61553f23381330349068f181b1
gilosbo <gilbert.osborne@gmail.com>
2019-08-28 14:26:25 -0700

    initial commit

---
+++ deckOfCards.py
@@ -0,0 +1,20 @@
+print("Hello World")
+
+class Card(object):
+    def __init__(self, suit, val):
+        self.suit = suit
+        self.value = val
+
+    def show(self):
+        print("{} of {}".format(self.value, self.suit))
+
+
+# class Deck(object):
+#     def __init__(self):
+#
+# class Player(object):
+#     def __init__(self):
+
+
+card = Card("Clubs", 6)
+card.show()
